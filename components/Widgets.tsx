ad1d83c5af97e6730b51d5db7b659260d136aaac
Jay Lawrence <dimaanojaylawrence@gmail.com>
2022-10-01 03:45:08 +0800

    Widgets margin

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -6,7 +6,7 @@ function Widgets() {
   return (
     <div className='px-2 mt-2 col-span-2 lg:inline hidden'>
         {/* Search */}
-        <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2 text-gray-400'>
+        <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2 mb-2 text-gray-400'>
             <MagnifyingGlassIcon className='h-5 w-5'/>
             <input 
                 className='flex-1 outline-none bg-transparent' 
