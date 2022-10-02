1d5e5c0ef6a65b07aba2df85747f146aa86f4900
Jay Lawrence <dimaanojaylawrence@gmail.com>
2022-10-03 02:39:06 +0800

    Corrections

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,6 @@ export const authOptions = {
     TwitterProvider({
         clientId: process.env.TWITTER_CLIENT_ID,
         clientSecret: process.env.TWITTER_CLIENT_SECRET,
-        version: '2.0'
     })
     // ...add more providers here
   ],
