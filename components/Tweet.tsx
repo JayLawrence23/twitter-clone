ce4cdfe62d8b9b4aad76eedebdc13f6141bb419c
Jay Lawrence <dimaanojaylawrence@gmail.com>
2022-10-17 00:26:42 +0800

    Comment Box

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -61,12 +61,11 @@ function Tweet({ tweet }: Props) {
     }
   return (
     <div className='flex flex-col space-x-3 border-y p-5 border-gray-100 cursor-pointer hover:bg-gray-50'>
-      
+         <Link href={`/tweet/${tweet._id}`}>
             <div className='flex space-x-3'>
                 <img className='h-10 w-10 rounded-full object-cover' src={tweet.profileImg} alt="" />
-              
+
                 <div>
-                <Link href={`/tweet/${tweet._id}`}>
                     <div className='flex items-center space-x-1'>
                         <p className='mr-1 font-bold'>{tweet.username}</p>
                         <p className='hidden text-sm text-gray-500 sm:inline'>@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
@@ -79,7 +78,7 @@ function Tweet({ tweet }: Props) {
                     { tweet.image && (
                         <img src={tweet.image} alt="" className='m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm' 
                     />)}
-                </Link>
+
                     <div className='flex justify-between mt-5 text-gray-500'>
                         <div 
                             onClick={() => setCommentBoxVisible(!commentBoxVisible)}
@@ -100,7 +99,7 @@ function Tweet({ tweet }: Props) {
                     </div>
                 </div>
             </div>
-      
+        </Link>
 
         {/*Comment Box Logic */}
         {commentBoxVisible && (
