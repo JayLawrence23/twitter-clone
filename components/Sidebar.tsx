32b50423119254354983548dabe1559ff3a5d377
Jay Lawrence <dimaanojaylawrence@gmail.com>
2022-10-21 01:24:05 +0800

    Tweet Image

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,8 @@ import {
     EnvelopeIcon,
     ClipboardDocumentListIcon,
     HomeIcon,
-    EllipsisHorizontalCircleIcon
+    EllipsisHorizontalCircleIcon,
+    GlobeAltIcon
 } from '@heroicons/react/24/outline'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link'
@@ -20,11 +21,12 @@ function Sidebar() {
 
   return (
     <div className='col-span-2 flex flex-col items-center px-4 md:items-start'>
-        <Link href="/">
-          <a> 
-          <img className='m-3 h-10 w-10' src="https://links.papareact.com/drq" alt="logo" />
-          </a>
-        </Link>
+          <Link href="/">
+            <a className='flex items-center'> 
+            <img className='m-3 h-10 w-10' src="https://links.papareact.com/drq" alt="logo" />
+            <span className='font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-pink-500'>KPR</span>
+            </a>
+          </Link>
         { session && (
             <>
             <SidebarRow Icon={HomeIcon} title="Home"/>
@@ -37,6 +39,12 @@ function Sidebar() {
         )
         }
         <SidebarRow onClick={session ? signOut : signIn} Icon={UserIcon} title={session ? 'Sign Out' : 'Sign In'}/>
+        <a href="https://kprverse.com/" target="_BLANK">
+          <SidebarRow Icon={GlobeAltIcon} title="kprverse.com"/>
+        </a> 
+        <a href="https://twitter.com/KPRVERSE" target="_BLANK">
+          <SidebarRow Icon={BookmarkIcon} title="KPR"/>
+        </a> 
 
         <SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More"/>
     </div>
